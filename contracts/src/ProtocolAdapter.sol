725ac3677ddd605273094821159b84650893bb08
Michael Heuer <michael.heuer@rwth-aachen.de>
2026-07-30 20:49:17 +0200

    chore: switch version to 2.0.0-alpha.0

--- contracts/src/ProtocolAdapter.sol
+++ contracts/src/ProtocolAdapter.sol
@@ -142,7 +142,7 @@ contract ProtocolAdapter is
 
     /// @inheritdoc IVersion
     function getVersion() external pure override returns (bytes32 version) {
-        version = "2.0.0-rc.0";
+        version = "2.0.0-alpha.0";
     }
 
     /// @inheritdoc IProtocolAdapter
