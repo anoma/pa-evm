6afac086cb890cc8762e85da8d88efed9d7ff3a6
Michael Heuer <michael.heuer@rwth-aachen.de>
2026-07-31 17:36:24 +0200

    chore: switch the version to 2.0.0-alpha.2
    Increment the alpha per landed migration step and clear the deployments map
    per the release checklist: the alpha.1 base-sepolia deployment is superseded
    and unaddressable, since the action struct change moved the execute selector.

--- contracts/src/ProtocolAdapter.sol
+++ contracts/src/ProtocolAdapter.sol
@@ -137,7 +137,7 @@ contract ProtocolAdapter is
 
     /// @inheritdoc IVersion
     function getVersion() external pure override returns (bytes32 version) {
-        version = "2.0.0-alpha.1";
+        version = "2.0.0-alpha.2";
     }
 
     /// @inheritdoc IProtocolAdapter
