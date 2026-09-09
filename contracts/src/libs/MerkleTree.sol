28d4b103aa4467c67a3f5f76a3d0491f05926eb9
Michael Heuer <michael.heuer@rwth-aachen.de>
2026-09-09 17:32:42 +0200

    refactor(contracts): move the test-only root helpers out of MerkleTree

--- contracts/src/libs/MerkleTree.sol
+++ contracts/src/libs/MerkleTree.sol
@@ -2,8 +2,6 @@
 pragma solidity ^0.8.30;
 
 import {Arrays} from "@openzeppelin-contracts-5.7.0/utils/Arrays.sol";
-import {Math} from "@openzeppelin-contracts-5.7.0/utils/math/Math.sol";
-import {SafeCast} from "@openzeppelin-contracts-5.7.0/utils/math/SafeCast.sol";
 
 import {SHA256} from "../libs/SHA256.sol";
 
@@ -14,8 +12,6 @@ import {SHA256} from "../libs/SHA256.sol";
 /// (https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.4.0/contracts/utils/structs/MerkleTree.sol).
 /// @custom:security-contact security@anoma.foundation
 library MerkleTree {
-    using SafeCast for uint256;
-
     struct Tree {
         uint256 _nextLeafIndex;
         bytes32[] _sides;
@@ -116,51 +112,5 @@ library MerkleTree {
     /// @return isLeft Whether this node is the left or right child.
     function isLeftChild(uint256 index) internal pure returns (bool isLeft) {
         isLeft = (index & 1) == 0;
-    }
-
-    /// @notice Computes the root of a Merkle tree.
-    /// @param leaves The leaves of the tree.
-    /// @param treeDepth The depth of the tree.
-    /// @return root The computed root.
-    /// @dev This method should only be used for trees with low depth.
-    function computeRoot(bytes32[] memory leaves, uint8 treeDepth) internal pure returns (bytes32 root) {
-        uint256 treeCapacity = uint256(1) << treeDepth; // 2^treeDepth
-
-        // Create array of full leaf set with padding if necessary
-        bytes32[] memory nodes = new bytes32[](treeCapacity);
-        for (uint256 i = 0; i < treeCapacity; ++i) {
-            if (i < leaves.length) {
-                nodes[i] = leaves[i];
-            } else {
-                nodes[i] = SHA256.EMPTY_HASH;
-            }
-        }
-
-        // Build the tree upward
-        uint256 currentLevelCapacity = treeCapacity;
-        while (currentLevelCapacity > 1) {
-            currentLevelCapacity /= 2;
-
-            for (uint256 i = 0; i < currentLevelCapacity; ++i) {
-                nodes[i] = SHA256.hash(nodes[2 * i], nodes[2 * i + 1]);
-            }
-        }
-
-        root = nodes[0];
-    }
-
-    /// @notice Computes the root of a Merkle tree using the minimal tree depth to fit all leaves.
-    /// @param leaves The leaves of the tree.
-    /// @return root The computed root.
-    /// @dev This method should only be used for trees with low depth.
-    function computeRoot(bytes32[] memory leaves) internal pure returns (bytes32 root) {
-        root = MerkleTree.computeRoot({leaves: leaves, treeDepth: computeMinimalTreeDepth(leaves.length)});
-    }
-
-    /// @notice Computes the minimal required tree depth for a number of leaves.
-    /// @param leavesCount The number of leaves.
-    /// @return treeDepth The minimal required tree depth.
-    function computeMinimalTreeDepth(uint256 leavesCount) internal pure returns (uint8 treeDepth) {
-        treeDepth = Math.log2({value: leavesCount, rounding: Math.Rounding.Ceil}).toUint8();
     }
 }
