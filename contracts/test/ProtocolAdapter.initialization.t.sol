0cc59c1a47bc0c1551eb15e9b8257c79c396db19
Michael Heuer <michael.heuer@rwth-aachen.de>
2026-09-10 15:22:30 +0200

    chore: use anoma-risc0-deployments 1.2.2

--- contracts/test/ProtocolAdapter.initialization.t.sol
+++ contracts/test/ProtocolAdapter.initialization.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.30;
 
 import {ERC1967Proxy} from "@openzeppelin-contracts-5.7.0/proxy/ERC1967/ERC1967Proxy.sol";
 import {Initializable} from "@openzeppelin-contracts-5.7.0/proxy/utils/Initializable.sol";
-import {DeployRiscZeroContractsMock} from "anoma-risc0-deployments-1.2.1/test/script/DeployRiscZeroContractsMock.s.sol";
+import {DeployRiscZeroContractsMock} from "anoma-risc0-deployments-1.2.2/test/script/DeployRiscZeroContractsMock.s.sol";
 import {Test} from "forge-std-1.16.2/src/Test.sol";
 import {Options} from "openzeppelin-foundry-upgrades-0.4.2/src/Options.sol";
 import {Upgrades} from "openzeppelin-foundry-upgrades-0.4.2/src/Upgrades.sol";
